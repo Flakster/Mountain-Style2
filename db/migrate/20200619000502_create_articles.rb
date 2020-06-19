abad11677692c35878d5cbcee9dad1d5c1893d84
flakster <carlos.santamaria@gmail.com>
2020-06-18 19:25:26 -0500

    Add foreign key author to Articles model

--- db/migrate/20200619000502_create_articles.rb
+++ db/migrate/20200619000502_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
       t.string :title
       t.text :body
       t.string :image
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
